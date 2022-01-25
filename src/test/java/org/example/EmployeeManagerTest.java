cea12673079e5c283f2d65378ff562162507e0ff
Madelene <madelene.axblom@gmail.com>
2022-01-25 20:15:07 +0100

    Removed faulty test

--- src/test/java/org/example/EmployeeManagerTest.java
+++ src/test/java/org/example/EmployeeManagerTest.java
@@ -18,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
-
     EmployeeRepository employeeRepository = new EmployeeRepositoryStub(List.of(
             new Employee("1", 50000),
             new Employee("2", 30000)));
@@ -37,29 +36,6 @@ class EmployeeManagerTest {
 
     }
 
-    @Test
-    @Disabled
-    void findAllEmployees() {
-
-        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-        BankService bankService = mock(BankService.class);
-
-        Employee emp1 = new Employee("1", 30000);
-        Employee emp2 = new Employee("2", 20000);
-        List<Employee> Employees = List.of(emp1, emp2);
-
-
-    }
-
-    @Test
-    void employeeGetTheRightSalary() {
-        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
-        BankService bankService = mock(BankService.class);
-        Employee emp1 = new Employee(anyString(), 30000);
-        Employee emp2 = new Employee(anyString(), 20000.0);
-        var result = Double.valueOf(emp1.getSalary());
-        assertEquals(30000, result);
-    }
 
     @Test
     void employeeIsPaid() {
